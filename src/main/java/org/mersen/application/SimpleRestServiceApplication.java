6ee7c0424db875e34fc3a951ab95bbde49e2ecf2
mustafa.ersen <mustafa.ersen@partner.bmwgroup.com>
2024-03-28 22:27:34 +0300

    initial commit2

--- src/main/java/org/mersen/application/SimpleRestServiceApplication.java
+++ src/main/java/org/mersen/application/SimpleRestServiceApplication.java
@@ -3,7 +3,7 @@ package org.mersen.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.mersen")
 public class SimpleRestServiceApplication {
 
     public static void main(String[] args) {
